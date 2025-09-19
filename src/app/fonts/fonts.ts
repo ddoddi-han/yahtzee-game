b7df7a937482b10436ab36757fc49564a4305996
sjhan <hsj6660@naonworks.com>
2025-09-19 15:21:14 +0900

    [fix] 프록시 문제로 구글폰트 -> 로컬폰트로 변경

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,49 @@
+import localFont from "next/font/local";
+
+export const geistSans = localFont({
+  src: [
+    {
+      path: "./Geist-Regular.woff2",
+      weight: "400",
+    },
+    {
+      path: "./Geist-Medium.woff2",
+      weight: "500",
+    },
+    {
+      path: "./Geist-SemiBold.woff2",
+      weight: "600",
+    },
+    {
+      path: "./Geist-Bold.woff2",
+      weight: "700",
+    },
+  ],
+  display: "swap",
+  preload: true,
+  variable: "--font-geist-sans",
+});
+
+export const geistMono = localFont({
+  src: [
+    {
+      path: "./GeistMono-Regular.woff2",
+      weight: "400",
+    },
+    {
+      path: "./GeistMono-Medium.woff2",
+      weight: "500",
+    },
+    {
+      path: "./GeistMono-SemiBold.woff2",
+      weight: "600",
+    },
+    {
+      path: "./GeistMono-Bold.woff2",
+      weight: "700",
+    },
+  ],
+  display: "swap",
+  preload: true,
+  variable: "--font-geist-mono",
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { geistSans, geistMono } from "./fonts/fonts";
 
 export const metadata: Metadata = {
   title: "Yathzee!",
